5cdc4c0248793eb025a8fd7bf14046c4a7b1fb0c
DarceLMTY <darce@latticemty.com.mx>
2024-07-03 00:52:44 -0600

    commit

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
 //Parameters
-//import fs from "fs";
-//import express from "express";
+
 const express = require('express');
 const app = express();
 const fs = require('fs');
 const port = process.env.API_PORT || 80;
 const API = require('./Middleware/apikeys');
-//import {validateKey}  from './Middleware/apikeys.js';
 app.use(express.json());
 
-// Temporal Data
-/*const associates = [
-    {ID_Associate: 111111111111, fLastName: 'Herrera', mLastName: 'Cardenas', Name: 'Diego', Credit: 0 ,  associate: true},
-    {ID_Associate: 222222222222, fLastName: 'Lozano', mLastName: 'Rubio', Name: 'Andres', Credit: 111111111.00, associate: false},
-    {ID_Associate: 333333333333, fLastName: 'Delgado', mLastName: 'Vazquez', Name: 'Monica', Credit: 999999999.00, associate: true},
-];*/
+
 
 const readData = () => {
     try {
@@ -46,8 +39,6 @@ app.get('/', (req, res)=>{
 app.get('/api/associates', API.validateKey, (req,res)=>{
 
     const data = readData();
-    //res.send(associates);
-    //res.json(data.associates);
     res.status(200).json(data.associates);
 
 });
@@ -67,35 +58,9 @@ app.get('/api/associates/:ID_Associate', API.validateKey, (req, res)=>{
 
 });
 
-/*
-//Get Associates
-app.get('/api/associates', (req,res)=>{
-
-    const data = readData();
-    //res.send(associates);
-    res.json(data.associates);
-
-});
-
-//Get Associate by ID
-app.get('/api/associates/:ID_Associate', (req, res)=>{
-    const data = readData();
-    const id = parseInt(req.params.ID_Associate);
-    const associate = data.associates.find((associate) => associate.ID_Associate === id);
-
-    //const associate = associates.find(c => c.ID_Associate === parseInt(req.params.ID_Associate));
-    if(!associate) return res.status(404).send('Associate NOT found');
-    else res.send(associate);
-});
-*/
 
 // The resource does not exist
 app.all('*',(req,res,next)=>{
-
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Cant find ${req.originalUrl} on the server`
-    // });
 
     res.status(404).send(`Cant find ${req.originalUrl} on the server`);
 
@@ -119,4 +84,3 @@ app.listen(port, function (err) {
     }
     console.log(`Listening on port ${port}`);
   });
-//app.listen(port,()=>console.log(`Escuchando en puerto ${port}...`));
